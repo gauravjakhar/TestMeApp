54943b9b2a4719a8e0ee525d9eb66b593a8cbfac
g.bw.kumar@accenture.com <Gaurav Jakhar>
2019-08-08 12:19:31 +0530

    First checkin to TestmeApp testing framework.

---
+++ Cucumber/resources/LoginFeature.feature
@@ -0,0 +1,8 @@
+Feature: Login Fuctionality
+
+@Login
+Scenario: Login with valid Credentials
+
+Given Navigate to home page
+When user enters username and password
+Then user logged in successfully
---
+++ Cucumber/src/main/java/Cucumber/LoginFeatureStepDefinition.java
@@ -0,0 +1,26 @@
+package Cucumber;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class LoginFeatureStepDefinition {
+	
+	
+
+	@Given("^Navigate to home page$")
+	public void navigateToHomePage() throws Throwable {
+		
+	}
+
+	@When("^user enters username and password$")
+	public void userEntersUsernameAndPassword() throws Throwable {
+		
+	}
+
+	@Then("^user logged in successfully$")
+	public void userLoggedInSuccessfully() throws Throwable {
+		
+	}
+
+}
